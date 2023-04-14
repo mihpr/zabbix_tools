9d83281f246be78eb758c8deb3b3ff087f4df1e7
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-04-14 12:00:36 +0300

    updated descriptions in trapper scripts

--- trap/trap1_stop.sh
+++ trap/trap1_stop.sh
@@ -2,4 +2,7 @@
 # Type: Zabbix trapper
 # Key: trap1
 # Type: text
+
+# Configure trigger having:
+# Expression: {Zabbix server:trap1.last()}="problem start"
 zabbix_sender -z localhost -p 10051 -s "Zabbix server" -k trap1 -o "problem stop"
