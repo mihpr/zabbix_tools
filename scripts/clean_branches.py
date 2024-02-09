5d150b224943ccefdb5cf315b5ac0a1e72d0740b
Mihails Prihodko <mihails.prihodko@gmail.com>
2024-02-09 09:59:54 +0200

    added script for cleaning branches

---
+++ scripts/clean_branches.py
@@ -0,0 +1,17 @@
+import os
+
+# 1. Define branches to delete
+# 2. Copy script to the repo.
+# 3. Run script: python clean_branches.py
+
+branches_to_delete = (
+    "feature/DEV-1224-6.0",
+    "feature/ZBX-1224-6.0",
+    "feature/ZBXNEXT-1224-6.0",
+)
+
+for branch in branches_to_delete:
+    os.system("git branch -D %s" % branch)
+
+print("Branches afte deletion:")
+os.system("git branch")
