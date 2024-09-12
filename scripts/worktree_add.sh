afce32f4515436c52109e4d289596bcdc522ca6a
Mihails Prihodko <mihails.prihodko@gmail.com>
2024-09-12 13:40:31 +0300

    fixed and added doc to worktree add script

--- scripts/worktree_add.sh
+++ scripts/worktree_add.sh
@@ -2,6 +2,11 @@
 
 # Run this script from the repository dir.
 
+# Basic usage example:
+# Navigate to the mail repository dir
+# Checkout and pull the release branch and run the script like this:
+# worktree_add.sh ZBXNEXT-9397-7.0
+
 WORKTREE_DIR=$1
 BRANCH=$2
 
@@ -13,7 +18,8 @@ CURRENT_DIR=$(pwd)
 WEBROOT=/var/www/html
 
 
-git worktree add --track -b ${BRANCH} "${CURRENT_DIR}/../${WORKTREE_DIR}" remotes/origin/${BRANCH}
+git worktree add -b ${BRANCH} "${CURRENT_DIR}/../${WORKTREE_DIR}"
+git push -u origin ${BRANCH}
 
 ln -s "${CURRENT_DIR}/../${WORKTREE_DIR}" "${WEBROOT}/${WORKTREE_DIR}"
 ls -l "${WEBROOT}/${WORKTREE_DIR}"
