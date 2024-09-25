f8d94958168a21a70bc5a2943c315e96bce9b406
Mihails Prihodko <mihails.prihodko@gmail.com>
2024-09-25 17:43:01 +0300

    updated valgrind start with HA mode and unified installation path

--- scripts/valgrind_start.sh
+++ scripts/valgrind_start.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-INSTALL_DIR="$PWD/install"
+INSTALL_DIR="$PWD"
 CONFIG_DIR="/usr/local/etc"
 
 rm -rf /tmp/valgrind
 mkdir -p /tmp/valgrind
 
+# basic start
+
 valgrind --leak-check=full --leak-resolution=high --show-leak-kinds=all --track-origins=yes --dsymutil=yes --error-limit=no --verbose --log-file=/tmp/valgrind/zabbix_server_%p ${INSTALL_DIR}/sbin/zabbix_server --config ${CONFIG_DIR}/zabbix_server.conf
+
+# start in HA mode (active node)
+
+# mkdir -p /tmp/ha1
+# mkdir -p /tmp/ha2
+# mkdir -p /tmp/ha3
+
+# valgrind --leak-check=full --leak-resolution=high --show-leak-kinds=all --track-origins=yes --dsymutil=yes --error-limit=no --verbose --log-file=/tmp/valgrind/zabbix_server_%p ${INSTALL_DIR}/sbin/zabbix_server --config ${CONFIG_DIR}/zabbix_server_ha1.conf
