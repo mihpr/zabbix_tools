258d07e9d0848f4b833dadb2ef4df87be9e5b560
Mihails Prihodko <mihails.prihodko@gmail.com>
2025-02-12 11:36:30 +0200

    improved mysql db restore script

--- scripts/myrestore.sh
+++ scripts/myrestore.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
+# ----------------------------------------------------------------------------------------------------------------------
+# settings
+# ----------------------------------------------------------------------------------------------------------------------
 DB_HOST=localhost
 DB_USER="zabbix"
-DB_NAME="zabbix"
 DB_PASSWORD="password"
-
 DB_DROP=true
+IS_ZABBIX_GT_5=true  # Set to true if Zabbix > 5.0, false otherwise
 
-# drop database
-if [ "${DB_DROP}" = true ]; then
-    mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="drop database if exists ${DB_NAME};"
-fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------------------------------------------------
+restore_db() {
+    local DB_NAME=$1
 
-# Zabbix > 5.0
-# mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="create database ${DB_NAME} character set utf8mb4 collate utf8mb4_bin;"
-# Zabbix  5.0
-mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="create database ${DB_NAME} character set utf8 collate utf8_bin;"
+    # Drop database if DB_DROP is true
+    if [ "${DB_DROP}" = true ]; then
+        mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="drop database if exists ${DB_NAME};"
+    fi
 
-mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} < ~/dumps/my_${DB_NAME}.sql
+    # Create database with utf8mb4 character set and collation for Zabbix > 5.0
+    if [ "${IS_ZABBIX_GT_5}" = true ]; then
+        mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="create database ${DB_NAME} character set utf8mb4 collate utf8mb4_bin;"
+    else
+        mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --execute="create database ${DB_NAME} character set utf8 collate utf8_bin;"
+    fi
+
+    # Import the database dump
+    mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} < ~/dumps/my_${DB_NAME}.sql
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------------------------------------------------
+
+restore_db "zabbix_int"
+restore_db "zabbix_int_proxy"
