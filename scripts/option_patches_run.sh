8a3b9dd1a376b29a68b4139081e4fa1ebcfbbf4a
Mihails Prihodko <mihails.prihodko@gmail.com>
2025-04-29 13:36:55 +0300

    improved option patches run script

--- scripts/option_patches_run.sh
+++ scripts/option_patches_run.sh
@@ -3,8 +3,15 @@
 DB_USER="zabbix"
 DB_NAME="deprecated"
 
+make dbschema
+
+# For upgrades
+cat database/postgresql/timescaledb/schema.sql | sudo -u $DB_USER psql $DB_NAME
+
+# Rename tables (compression enabled)
 cat database/postgresql/timescaledb/option-patches/with-compression/history_upgrade_prepare.sql | sudo -u $DB_USER psql $DB_NAME
 
+# Hypertable migration scripts (compression enabled)
 cat database/postgresql/timescaledb/option-patches/with-compression/history_upgrade.sql | sudo -u $DB_USER psql $DB_NAME
 cat database/postgresql/timescaledb/option-patches/with-compression/history_upgrade_uint.sql | sudo -u $DB_USER psql $DB_NAME
 cat database/postgresql/timescaledb/option-patches/with-compression/history_upgrade_log.sql | sudo -u $DB_USER psql $DB_NAME
