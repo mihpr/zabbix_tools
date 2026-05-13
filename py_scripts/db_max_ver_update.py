0519d0bdbaa14c1cfd47aabe48e85d6a503c6557
Mihails Prihodko <mihails.prihodko@gmail.com>
2026-05-13 17:51:51 +0300

    updated max ver update script

--- py_scripts/db_max_ver_update.py
+++ py_scripts/db_max_ver_update.py
@@ -1,5 +1,12 @@
 import os
 import re
+import sys
+
+### constants
+DB_TYPE_MYSQL = "MySQL"
+DB_TYPE_TIMESCALE = "TimescaleDB"
+
+### settings
 
 # (release branch, feature branch suffix)
 git_branch_sfx = (
@@ -10,12 +17,24 @@ git_branch_sfx = (
 	("master",      "7.5")
 )
 
-jira= "ZBXNEXT-10448"
-db_type="MySQL"
-new_db_max_version_str1="9.6"
-new_db_max_version_str2="9.6.x"
-new_db_max_version_int="90699"
-author="mprihodko"
+jira    = "ZBXNEXT-10618"
+db_type = DB_TYPE_TIMESCALE
+author  = "mprihodko"
+
+if db_type == DB_TYPE_MYSQL:
+    new_db_max_version_str1 = "9.7"
+    new_db_max_version_str2 = "9.7.x"
+    new_db_max_version_int  = "90799"
+elif db_type == DB_TYPE_TIMESCALE:
+    new_db_max_version_str1 = "2.27"
+    new_db_max_version_str2 = new_db_max_version_str1
+    new_db_max_version_int  = "22799"
+else:
+    print(f"Error: unsupported db_type '{db_type}'")
+    sys.exit(1)
+
+
+### program start
 
 os.system("git reset --hard")
 os.system("git clean -dfx")
@@ -26,11 +45,16 @@ for rel_branch, feat_sfx in git_branch_sfx:
     feat_branch = "feature/{}-{}".format(jira, feat_sfx)
 
     os.system("git checkout {}".format(rel_branch))
+    os.system("git pull")
     os.system("git checkout -b {}".format(feat_branch))
 
     file_path = "ChangeLog.d/feature/{}".format(jira)
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    commit_msg = "A......PS. [{}] updated maximum supported {} version to {}".format(jira, db_type, new_db_max_version_str1)
+    if db_type == DB_TYPE_MYSQL:
+        components = 'A......PS.'
+    elif db_type == DB_TYPE_TIMESCALE:
+        components = 'A.......S.'
+    commit_msg = "{} [{}] updated maximum supported {} version to {}".format(components, jira, db_type, new_db_max_version_str1)
     changelog = "{} ({})\n".format(commit_msg, author)
 
     with open(file_path, "w") as f:
@@ -43,17 +67,30 @@ for rel_branch, feat_sfx in git_branch_sfx:
     with open(file_path, "r") as f:
         content = f.read()
 
-    content = re.sub(
-        r"(#define ZBX_MYSQL_MAX_VERSION\s+)\d+",
-        rf"\g<1>{new_db_max_version_int}",
-        content
-    )
+    if db_type == DB_TYPE_MYSQL:
+        content = re.sub(
+            r"(#define ZBX_MYSQL_MAX_VERSION\s+)\d+",
+            rf"\g<1>{new_db_max_version_int}",
+            content
+        )
 
-    content = re.sub(
-        r'(#define ZBX_MYSQL_MAX_VERSION_STR\s+)"[^"]+"',
-        rf'\g<1>"{new_db_max_version_str2}"',
-        content
-    )
+        content = re.sub(
+            r'(#define ZBX_MYSQL_MAX_VERSION_STR\s+)"[^"]+"',
+            rf'\g<1>"{new_db_max_version_str2}"',
+            content
+        )
+    elif db_type == DB_TYPE_TIMESCALE:
+        content = re.sub(
+            r"(#define ZBX_TIMESCALE_MAX_VERSION\s+)\d+",
+            rf"\g<1>{new_db_max_version_int}",
+            content
+        )
+
+        content = re.sub(
+            r'(#define ZBX_TIMESCALE_MAX_VERSION_STR\s+)"[^"]+"',
+            rf'\g<1>"{new_db_max_version_str2}"',
+            content
+        )
 
     with open(file_path, "w") as f:
         f.write(content)
